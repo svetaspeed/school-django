e73749789b319995863337d734edf11fe76f2580
svetaspeed <svetaspeedster@gmail.com>
2021-07-17 22:11:51 +1000

    lesson 3

--- school/mainapp/models.py
+++ school/mainapp/models.py
@@ -1,1 +1,21 @@
 from django.db import models
+
+
+class News(models.Model):
+    title = models.CharField('Заголовок', max_length=50)
+    text = models.TextField('Текст')
+
+
+    def __str__(self):
+        return self.title
+
+
+class Review(models.Model):
+    vote = models.SmallIntegerField()
+    user = models.CharField('Имя', max_length=50)
+    text = models.TextField('Отзыв', null=True)
+    voted_on = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
