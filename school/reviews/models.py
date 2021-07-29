a6917d9dcff7284129af50fafb88bebbfa57e185
svetaspeed <svetaspeedster@gmail.com>
2021-07-29 21:59:45 +1000

    lesson 5

--- school/reviews/models.py
+++ school/reviews/models.py
@@ -4,8 +4,7 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 class Rating(models.Model):
-    vote = models.SmallIntegerField()
-    name = models.CharField('Имя', max_length=50)
+    name = models.CharField('Имя', max_length=50, default='name')
     text = models.TextField('Отзыв', null=True)
     score = models.IntegerField(default=0,
         validators=[
@@ -15,7 +14,7 @@ class Rating(models.Model):
     )
 
     def __str__(self):
-        return str(self.vote)
+        return str(self.name)
 
     class Meta:
         verbose_name = 'Отзыв'
