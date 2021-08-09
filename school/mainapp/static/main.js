5eaaa055d2e2c971b43f9d6f29fc2ac16d4b315e
svetaspeed <svetaspeedster@gmail.com>
2021-08-09 15:07:56 +1000

    lesson 6

---
+++ school/mainapp/static/main.js
@@ -0,0 +1,57 @@
+const one = document.getElementById('first')
+const two = document.getElementById('second')
+const three = document.getElementById('third')
+const four = document.getElementById('fourth')
+const five = document.getElementById('fifth')
+
+const form = document.querySelector('.rate-form')
+const confirmBox = document.getElementById('confirm-box')
+
+const csrf = document.getElementsByName('csrfmiddlewaretoken')
+
+const handleStarSelect = (size) => {
+    const children= form.children
+    for (let i=0; i < children.length; i++){
+        if (i <= size) {
+            children[i].classList.add('checked')
+        } else {
+            children[i].classList.remove('checked')
+        }
+    }
+}
+
+const handleSelect = (selection) => {
+    switch(selection){
+        case 'first': {
+//            one.classList.add('checked')
+//            two.classList.remove('checked')
+//            three.classList.remove('checked')
+//            four.classList.remove('checked')
+//            five.classList.remove('checked')
+            handleStarSelect(1)
+            return
+        }
+        case 'second': {
+            handleStarSelect(2)
+            return
+        }
+        case 'third': {
+            handleStarSelect(3)
+            return
+        }
+        case 'fourth': {
+            handleStarSelect(4)
+            return
+        }
+        case 'fifth': {
+            handleStarSelect(5)
+            return
+        }
+    }
+}
+
+const arr = [one, two, three, four, five]
+
+arr.forEach(item=> item.addEventListener('mouseover', (event)=>{
+    handleSelect(event.target.id)
+}))
