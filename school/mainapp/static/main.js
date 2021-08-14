168464ea1d61f1b4074542edfb7f48420f0715c9
svetaspeed <svetaspeedster@gmail.com>
2021-08-14 20:23:15 +1000

    lesson 6

--- school/mainapp/static/main.js
+++ school/mainapp/static/main.js
@@ -23,11 +23,6 @@ const handleStarSelect = (size) => {
 const handleSelect = (selection) => {
     switch(selection){
         case 'first': {
-//            one.classList.add('checked')
-//            two.classList.remove('checked')
-//            three.classList.remove('checked')
-//            four.classList.remove('checked')
-//            five.classList.remove('checked')
             handleStarSelect(1)
             return
         }
@@ -50,8 +45,18 @@ const handleSelect = (selection) => {
     }
 }
 
-const arr = [one, two, three, four, five]
+if (one) {
+    const arr = [one, two, three, four, five]
 
-arr.forEach(item=> item.addEventListener('mouseover', (event)=>{
-    handleSelect(event.target.id)
-}))
+    arr.forEach(item=> item.addEventListener('mouseover', (event)=>{
+        handleSelect(event.target.id)
+    }))
+
+    arr.forEach(item=> item.addEventListener('click', (event)=>{
+        const val = event.target.id
+
+        form.addEventListener('submit', e=>{
+            e.preventDefault()
+        })
+    }))
+}
